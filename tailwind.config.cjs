34d3dc0e14d0d302ac6dd6de239c2b79eb38d415
carlosllinas <soportehackcm@gmail.com>
2023-07-15 16:33:10 -0500

    15/07/23

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,9 @@ module.exports = {
     content: ['./src/**/*.{html,js,astro,jsx,tsx,vue,svelte}'],
     theme: {
         extend: {
-
+            gradientColorStopPositions: {
+                finalDegradado: 'linear-gradient (171deg, rgba(49,15,66,1) 0%, rgba(72,30,78,1) 13%, rgba(48,37,80,1) 34%, rgba(0,0,0,1) 84%)'
+            }
         },
         fontFamily: {
             'sg': ['Space Grotesk', 'sans-serif']
